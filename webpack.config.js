0aea3d6b3507c95fa6e1b12423fc5ba28a5588a7
maslianok <maslianok@gmail.com>
2018-04-02 19:24:15 +0300

    Upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,20 @@
-'use strict';
 
-var webpack = require('webpack');
 
-var env = process.env.NODE_ENV;
-var config = {
+const webpack = require('webpack');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   module: {
-    loaders: [
-      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
-    ],
+    rules: [{ test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/ }]
   },
   output: {
     library: 'react-responsive-tabs',
-    libraryTarget: 'umd',
+    libraryTarget: 'umd'
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env)
-    }),
-  ],
-};
-
-if (env === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-        screw_ie8: false,
-      },
-      mangle: {
-        screw_ie8: false,
-      },
-      output: {
-        screw_ie8: false,
-      },
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
-  );
-}
-
-module.exports = config;
+  ]
+};
