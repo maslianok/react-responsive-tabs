c78a3314895d861cac1184910305ce70768d57e8
Vitalii Maslianok <maslianok@gmail.com>
2020-06-05 12:22:36 +0300

    Upgrade libraries

--- examples/src/App.js
+++ examples/src/App.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import ReactGA from 'react-ga';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import cs from 'classnames';
 
 import 'react-responsive-tabs/styles.css';
@@ -30,10 +30,19 @@ class App extends PureComponent {
       <div className="container">
         <Helmet
           title="React tabs"
-          script={[{ src: 'https://buttons.github.io/buttons.js', async: true, defer: true }]}
+          script={[
+            {
+              src: 'https://buttons.github.io/buttons.js',
+              async: true,
+              defer: true,
+            },
+          ]}
           meta={[
             { name: 'description', content: 'React responsive tabs' },
-            { property: 'keywords', content: 'react, tabs, responsive, accordion' },
+            {
+              property: 'keywords',
+              content: 'react, tabs, responsive, accordion',
+            },
           ]}
         />
         <div className="jumbotron">
@@ -55,13 +64,17 @@ class App extends PureComponent {
         </div>
         <div className="menu">
           <div
-            className={cs('menu-item', { 'menu-item--active': active === 'basic' })}
+            className={cs('menu-item', {
+              'menu-item--active': active === 'basic',
+            })}
             onClick={this.onChangeExample('basic')}
           >
             basic usage
           </div>
           <div
-            className={cs('menu-item', { 'menu-item--active': active === 'removal' })}
+            className={cs('menu-item', {
+              'menu-item--active': active === 'removal',
+            })}
             onClick={this.onChangeExample('removal')}
           >
             tabs removal
